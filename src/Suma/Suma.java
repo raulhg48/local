6f8de8f223ed462fe2e7e15c34293318da88537a
U_35007842 <U_35007842@DESKTOP-268RKIA>
2019-01-31 08:21:46 +0000

    Versión oroginal

---
+++ src/Suma/Suma.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Suma;
+
+/**
+ *
+ * @author Brian
+ */
+public class Suma { //Declaramos los atributos iniciales
+    int n1, n2, resultado ;
+   
+public Suma (int a, int b){ //Creamos un constructor para inicializar las variables a y b
+    this.n1 = a ;
+    this.n2 = b ;
+}
+public int sumar (){    //creamos un método (nos dará error hasta hacer un return)
+    this.resultado = this.n1 + this.n2 ;
+    return this.resultado ;
+}
+public static void main(String[] args) {    //añadimos un main para crear un nuevo objeto
+   Suma suma1 = new Suma(50,30) ;           // y más adelante proyectarlo en pantalla.
+    int resultado = suma1.sumar() ;
+    System.out.println("La suma es: "+resultado);    
+}
+}
